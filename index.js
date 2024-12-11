01bdaa964bd44a8f609c916465d39b16c74baa52
Lugard Agu <107192340+lugard1@users.noreply.github.com>
2024-12-11 20:54:00 +0100

    Add index.js file and grab the required elements

---
+++ index.js
@@ -0,0 +1,7 @@
+const imageContainerEl = document.querySelector('.image-container')
+
+const btnEl = document.querySelector('.btn')
+
+btnEl.addEventListener('click', ()=> {
+  console.log('clicked');
+})
